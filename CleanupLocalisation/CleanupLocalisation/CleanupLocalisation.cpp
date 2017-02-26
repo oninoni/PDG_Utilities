03dd5d398a6db2a1ca6cd26378983fa1175149cb
Dominik <Dominik@WSDOMI>
2017-02-26 16:42:52 +0100

    - added VanillaFiles to gitignore - spam is now greatly reduced and easier to read - empty strings are replaced by a string containing a newline - skipping "." and ".." from FindFile with a continue - renamed fileShouldOutput to fileChanged - renamed regexer to keyExp

--- CleanupLocalisation/CleanupLocalisation/CleanupLocalisation.cpp
+++ CleanupLocalisation/CleanupLocalisation/CleanupLocalisation.cpp
@@ -11,11 +11,13 @@ int main()
         "Output\\STH_l_english.yml"
     };
 
-    std::regex regexer(" +(.+?):0");
+    std::regex keyExp("^ +(.+?):[0-9]+");
+    std::regex emptystringExp("^( +.+?:[0-9]+ +)\\\"\\\"$");
     std::smatch matches;
 
+    std::cout << " --- Loading Tags ---" << std::endl;
+
     for (std::string fileName : STNHFiles) {
-        std::cout << "Loading " << fileName << std::endl;
         std::ifstream file(fileName);
         if (!file.is_open()) {
             std::cout << "Error opening \"" << fileName << "\"!! :(" << std::endl;
@@ -24,40 +26,43 @@ int main()
         std::string line;
         while (!file.eof()) {
             std::getline(file, line);
-            if (std::regex_search(line, matches, regexer, std::regex_constants::match_continuous)) {
+            if (std::regex_search(line, matches, identExp, std::regex_constants::match_continuous)) {
                 usedTags.insert(matches[1]);
             }
         }
         file.close();
         std::cout << "Loaded " << fileName << std::endl;
     }
-    
+   
     WIN32_FIND_DATA data;
     HANDLE h = FindFirstFile(L"VanillaFiles\\*.*", &data);
 
     if (h != INVALID_HANDLE_VALUE) {
+        std::cout << " --- Updating Files ---" << std::endl;
         do {
-            std::wstring fileName = _T("VanillaFiles\\") + std::wstring(data.cFileName);
-            bool fileShouldOutput = false;
+            if (!StrCmp(data.cFileName, _T(".")) || !StrCmp(data.cFileName, _T("..")))
+                continue;
 
-            std::wcout << "Loading " << fileName << std::endl;
+            std::wstring fileName = _T("VanillaFiles\\") + std::wstring(data.cFileName);
+            bool fileChanged = false;
+
             std::ifstream file(fileName);
             std::vector<std::string> fileData;
 
             if (!file.is_open()) {
-                std::wcout << "Error opening \"" << fileName << "\"!! :(" << std::endl;
+                std::wcout << "Error opening \"" << fileName << "\"!" << std::endl;
                 continue;
             }
 
             std::string line;
             while (!file.eof()) {
                 std::getline(file, line);
-                if (std::regex_search(line, matches, regexer, std::regex_constants::match_continuous)) {
+                if (std::regex_search(line, matches, identExp, std::regex_constants::match_continuous)) {
                     if (usedTags.find(matches[1]) == usedTags.end()) {
                         fileData.push_back(line);
                     } else {
-                        std::cout << "Ignoring " << matches[1] << std::endl;
-                        fileShouldOutput = true;
+                        //std::cout << "Ignoring " << matches[1] << std::endl;
+                        fileChanged = true;
                     }
                 } else {
                     fileData.push_back(line);
@@ -65,23 +70,21 @@ int main()
             }
 
             file.close();
-            std::wcout << "Loaded " << fileName << std::endl;
 
-            if (fileShouldOutput) {
+            if (fileChanged) {
                 fileName = _T("Output\\") + std::wstring(data.cFileName);
                 std::ofstream outFile(fileName);
-                for (std::string line : fileData) {
-                    outFile << line << std::endl;
-                }
+                for (std::string line : fileData) 
+                    outFile << std::regex_replace(line, emptystringExp, "$1\"\\n\"") << std::endl;
                 outFile.close();
-                std::wcout << "Saved " << fileName << std::endl;
+                std::wcout << "[UPDATED] \"" << fileName << "\"" << std::endl;
             } else {
-                std::wcout << "Not writing \"" << fileName << "\", because it's not used." << std::endl;
+                std::wcout << "[ignored] \"" << fileName << "\"" << std::endl;
             }
         } while (FindNextFile(h, &data));
     }
     else
-        std::cout << "Error: No such folder." << std::endl;
+        std::cout << "VanillaFiles folder missing!" << std::endl;
 
     FindClose(h);
 
